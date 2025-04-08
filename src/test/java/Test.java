99278fe30ffe2e9ea80e63d2a7931ab9955eeed2
Arun Kancharla <arunyoyo00@gmail.com>
2025-04-08 00:08:44 -0400

    Fixed compilation issue

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,11 +3,5 @@ import java.util.Arrays;
 public class Test {
 
     public static void main(String[] args) {
-        String test = "Check";
-        byte[] bytes = test.getBytes();
-        bytes.
-        System.out.println();
-        String format = String.format("These are the bytes: %s", bytes);
-        System.out.println(format);
     }
 }
